ac10c912c1e0ef2aa9f14cd2dd4245136d4886a8
Matías Szostak <93807332+matiasmszostak@users.noreply.github.com>
2025-01-03 20:26:00 -0300

    Tested the app with real emails.

--- main.py
+++ main.py
@@ -1,19 +1,15 @@
 import smtplib
 
 
-my_email = "xxxxxxxx@gmail.com"
-password = "abcd1234"
+my_email = "appdetesting@gmail.com"
+password = "************"
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user= my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs="xxxxxxxtest@yahoo.com", msg="Hello")
-connection.close()
-
-
-
-
-
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user= my_email, password=password)
+    connection.sendmail(from_addr=my_email,
+                        to_addrs="appdetesting@yahoo.com",
+                        msg="Subject:Hello\n\n This is the body of my email.")
 
 
 smtplib.SMTP("smtp.gmail.com", port=587)
